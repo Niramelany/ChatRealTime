e0375e33d5377a2236292847c9cd3b13235e456f
DavidPita <erick.pita.pincay@gmail.com>
2024-07-12 04:37:03 -0500

    v2.0-mejorar-interfaz

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,3 @@
-from flask import Blueprint
-
-
 def register_routes(app):
     from app.routes.chat_route.chat import chat_bp
     from app.routes.login_route.login import login_bp
--- app/routes/chat_route/chat.py
+++ app/routes/chat_route/chat.py
@@ -1,19 +1,40 @@
 from flask import Blueprint, request, render_template,redirect
 from app.extensions import socketio
-from flask_socketio import send
+from flask_socketio import send,emit
 
 chat_bp = Blueprint('chat', __name__)
-listUsers=[]
+listUsers={}
 
-@chat_bp.route('/', methods=['GET'])
+@chat_bp.route('/')
 def new_chat():
+    print(request.args.to_dict)
+    print(request.args.get('name'))
     currentUser = request.args.get('name')
     if not currentUser:
         return redirect('/')
-    listUsers.append(currentUser)
-    return render_template('chat.html',currentUser=currentUser,listUsers=listUsers)
+    return render_template('chat.html',currentUser=currentUser)
 
 @socketio.on('message')
 def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast = True)
+    print(f'Message de {request.sid}: {msg}')
+    emit('message-sent',{"user":listUsers.get(request.sid),'msg':msg}, room=request.sid,json=True)
+    emit('message-received',{"user":listUsers.get(request.sid),'msg':msg}, broadcast= True,json=True,include_self=False)
+
+@socketio.on('connect')
+def connect():
+    print(f"Nuevo cliente conectado: {request.sid}")
+    
+@socketio.on('user_connect')
+def user_connect(user):
+    listUsers[request.sid]=user
+    listu=[v for k, v in listUsers.items()]
+    emit('user_connect_list',{'user':listu},json=True,broadcast=True)
+    emit('user_connected',{'user':listUsers.get(request.sid)},json=True,broadcast=True,include_self=False)
+    
+@socketio.on('disconnect')
+def connect():
+    print(f"cliente desconectado: {listUsers.get(request.sid,'desconocido')}")
+    emit('user_disconnect',{'user':listUsers.get(request.sid,'desconocido')},json=True,broadcast=True)
+    listUsers.pop(request.sid)
+    listu=[v for k, v in listUsers.items()]
+    emit('user_connect',{'user':listu},json=True,broadcast=True)
